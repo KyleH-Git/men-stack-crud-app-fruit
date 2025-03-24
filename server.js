49df70973d8366e0aeb64c50b7d4a74569e1e656
Kyle Hayden <dahaydnator@gmail.com>
2025-03-24 10:21:44 -0500

    added initial setup, installed MongoDB/Express/Node, created fruit model, started views for practice site

---
+++ server.js
@@ -0,0 +1,51 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Connect to MongoDB using the connection string in the .env file
+mongoose.connect(process.env.MONGODB_URI);
+
+//log connection status to terminal on start
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
+//Import the Fruit model
+const Fruit = require('./models/fruit.js');
+app.use(express.urlencoded({extended: false}));
+
+
+
+// GET /
+app.get('/', async (req, res) => {
+    res.render('index.ejs');
+});
+
+// GET /fruits
+app.get('/fruits', async (req, res) => {
+    const allFruits = await Fruit.find();
+    res.render('fruits/index.ejs', {fruits: allFruits});
+});
+
+// GET /fruits/new
+app.get('/fruits/new', async (req, res) => {
+    res.render('fruits/new.ejs');
+});
+
+// POST /fruits
+app.post('/fruits', async (req, res) => {
+    if(req.body.isReadyToEat === 'on') {
+        req.body.isReadyToEat = true;
+    }else{
+        req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    res.redirect('/fruits');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on Port 3000...');
+});
